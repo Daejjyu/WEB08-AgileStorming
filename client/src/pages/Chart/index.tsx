7d26d49eff2fa17a08802f9d04afaf221644b714
SeungHyun Kim <daejjyu@gmail.com>
2021-11-25 05:59:45 +0900

    ✨ 차트 레이아웃 구현 #132
    중복 작업이므로 주영이 구현버전으로 통합 예정

--- client/src/pages/Chart/index.tsx
+++ client/src/pages/Chart/index.tsx
@@ -1,5 +1,45 @@
+import styled from '@emotion/styled';
+import { PriorityChart, DoneTaskChart } from 'components/organisms';
+import CommonLayout from 'components/templates/CommonLayout';
+
+const CenterContainer = styled.div`
+  ${({ theme }) => theme.absoluteCenter};
+  ${({ theme }) => theme.flex.columnCenter};
+  width: 80vw;
+  height: 80vh;
+  margin-top: 5vh;
+  gap: 5%;
+`;
+
+const Header = styled.div`
+  background-color: ${({ theme }) => theme.color.white};
+  border-radius: 0.5rem;
+  ${({ theme }) => theme.flex.rowCenter};
+  ${({ theme }) => theme.shadow};
+  width: 100%;
+  height: 10%;
+`;
+
+const ChartWrapper = styled.div`
+  background-color: ${({ theme }) => theme.color.white};
+  border-radius: 0.5rem;
+  ${({ theme }) => theme.shadow};
+  width: 100%;
+  height: 85%;
+`;
+
 const Chart = () => {
-  return <div>차트페이지</div>;
+  return (
+    <CommonLayout>
+      <CenterContainer>
+        <Header>헤더</Header>
+        <ChartWrapper>
+          <DoneTaskChart />
+          <PriorityChart />
+        </ChartWrapper>
+      </CenterContainer>
+    </CommonLayout>
+  );
 };
 
 export default Chart;
